8ae80b5ae85978e04f76b09cf38e2d97ed709838
Gokulaprasanth-Dev <gokulaprasanth9697.g@gmail.com>
2026-05-22 17:46:58 +0530

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Tracker - 1/modules/auth/protocols.py

--- Tracker - 1/modules/auth/protocols.py
+++ Tracker - 1/modules/auth/protocols.py
@@ -1,4 +1,5 @@
+from typing import Protocol
 from modules.auth.models.blacklisted_token_model import BlacklistedTokenModel
 
-class BlacklistedTokenWrite():
+class BlacklistedTokenWrite(Protocol):
     async def create_blacklisted_token(self,data: BlacklistedTokenModel): ...
