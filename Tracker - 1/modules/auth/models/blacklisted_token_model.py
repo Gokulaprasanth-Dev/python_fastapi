e490811d70e779e22cb2c8bbed3ccb80727670ad
Gokulaprasanth-Dev <gokulaprasanth9697.g@gmail.com>
2026-05-22 15:46:40 +0530

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Tracker - 1/modules/auth/models/blacklisted_token_model.py

--- Tracker - 1/modules/auth/models/blacklisted_token_model.py
+++ Tracker - 1/modules/auth/models/blacklisted_token_model.py
@@ -4,8 +4,12 @@ from datetime import datetime,timezone
 
 from pydantic import Field
 
+from uuid import UUID,uuid4
+
 class BlacklistedTokenModel(BaseSchema):
-    id:str
+    id:UUID = Field(default_factory=uuid4)
+    jti: str
+    user_id: str
     expires_at: datetime =Field(
         default_factory=lambda: datetime.now(timezone.utc)
     )
